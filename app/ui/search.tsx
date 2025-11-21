d10f4cf1005e3fae1eb978530d7b92b528c5b7a1
sulhanfuadi <sulhanfuadi096@gmail.com>
2025-11-21 15:26:30 +0700

    fix: add missing placeholder prop to Search component

--- app/ui/search.tsx
+++ app/ui/search.tsx
@@ -4,7 +4,7 @@ import { MagnifyingGlassIcon } from "@heroicons/react/24/outline";
 import { useSearchParams, usePathname, useRouter } from "next/navigation";
 import { useDebouncedCallback } from "use-debounce";
 
-export default function Search() {
+export default function Search({ placeholder }: { placeholder: string }) {
   const searchParams = useSearchParams();
   const pathname = usePathname();
   const { replace } = useRouter();
